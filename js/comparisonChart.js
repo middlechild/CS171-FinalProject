68f8ac41ac1c024a919125df1f121549b31158b6
Charlie Harrington <54407966+charlieh205@users.noreply.github.com>
2022-11-28 23:34:13 -0500

    Removing top margin from vis

--- js/comparisonChart.js
+++ js/comparisonChart.js
@@ -79,7 +79,7 @@ class ComparisonVis {
         let vis = this;
 
         // Calculate drawing space attributes
-        vis.margin = {top: 20, right: 20, bottom: 20, left: 20};
+        vis.margin = {top: 0, right: 20, bottom: 20, left: 20};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
